ba7f5d8656c164c5621f350ccfd7fce7a1ac110f
limdongsun0814 <118763659+limdongsun0814@users.noreply.github.com>
2023-10-20 22:22:50 +0900

    [Gold V] Title: 토마토, Time: 2856 ms, Memory: 432844 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,206 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	static int[][] map;
+	static int N;
+	static int M;
+	static int x;
+	static int y;
+	static HashMap<String, ArrayList<String>> graph;
+	static BufferedReader br;
+	static ArrayList<int[]> line;
+	static ArrayList<String> v;
+	static int result;
+	public static void main(String[] args) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		String[] data = br.readLine().split(" ");
+		// 6 4
+		M = Integer.parseInt(data[0]);
+		N = Integer.parseInt(data[1]);
+		map = new int[N][M];
+
+		make_map();
+
+		//print_map();
+
+		//make_line();
+
+		//print_line();
+
+		//make_graph();
+
+		//print_graph();
+
+		find_v();
+		
+		//print_v();
+		
+		bfs();
+		
+		System.out.println(has_zero()?-1:result);
+	}
+
+	static void make_map() throws IOException {
+		for (int i = 0; i < N; i++) {
+			String[] data = br.readLine().split(" ");
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(data[j]);
+			}
+		}
+	}
+
+	static void make_line() {
+		line = new ArrayList<int[]>();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (i + 1 < N) {
+					if (map[i][j] != -1 && map[i + 1][j] != -1) {
+						line.add(new int[] { i, j, i + 1, j });
+					}
+				}
+
+				if (j + 1 < M) {
+					if (map[i][j] != -1 && map[i][j + 1] != -1) {
+						line.add(new int[] { i, j, i, j + 1 });
+					}
+				}
+			}
+		}
+	}
+
+	static void make_graph() {
+		graph = new HashMap<String, ArrayList<String>>();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				graph.put(i + "," + j, new ArrayList<String>());
+			}
+		}
+
+		for (int[] xy : line) {
+			graph.get(xy[0] + "," + xy[1]).add(xy[2] + "," + xy[3]);
+			graph.get(xy[2] + "," + xy[3]).add(xy[0] + "," + xy[1]);
+		}
+	}
+
+	static void find_v() {
+		v = new ArrayList<String>();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 1) {
+					v.add(i + "," + j);
+					map[i][j]=1;
+				}
+			}
+		}
+	}
+
+	static boolean has_zero() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	static void bfs() {
+		Queue<String> q = new LinkedList<String>();
+		Queue<String> buffer = new LinkedList<String>();
+		for (String i : v) {
+			q.add(i);
+			buffer.add(i);
+		}
+		result=-1; // 자기 자신은 이미 익어서 -1
+		while (!buffer.isEmpty()) {
+			buffer.clear();
+			while (!q.isEmpty()) {
+				String val = q.poll();
+				y=Integer.parseInt(val.split(",")[0]);
+				x=Integer.parseInt(val.split(",")[1]);
+				ArrayList<String> flag = new ArrayList<String>();
+
+				if (y + 1 < N) {
+					if ( map[y + 1][x] == 0) {
+						flag.add((y+1)+","+x);
+					}
+				}
+
+				if (x + 1 < M) {
+					if ( map[y][x + 1] ==0) {
+						flag.add(y+","+(x+1));
+					}
+				}
+				
+				if (y - 1 >= 0) {
+					if (map[y - 1][x] ==0) {
+						flag.add(y-1+","+x);
+					}
+				}
+
+				if (x - 1 >= 0) {
+					if ( map[y][x - 1] == 0) {
+						flag.add(y+","+(x-1));
+					}
+				}
+				
+				
+				for (String nextVal : flag) {
+					y = Integer.parseInt(nextVal.split(",")[0]);
+					x = Integer.parseInt(nextVal.split(",")[1]);
+					if (map[y][x]!=1) {
+						buffer.add(nextVal);
+						map[y][x]=1;
+					}
+				}
+			}
+			q.addAll(buffer);
+			result++;
+		}
+		
+	}
+
+	static void print_map() {
+		System.out.println("=====print map start=====");
+		for (int i = 0; i < N; i++) {
+			System.out.println(Arrays.toString(map[i]));
+		}
+		System.out.println("====print map end======");
+	}
+
+	static void print_line() {
+		System.out.println("====print line start======");
+		for (int[] arr : line) {
+			System.out.println(Arrays.toString(arr));
+		}
+		System.out.println("=====print line end=====" + line.size());
+	}
+
+	static void print_graph() {
+		System.out.println("====print graph start======"+graph.size());
+		for (String arr : graph.keySet()) {
+			System.out.println(arr + " : " + graph.get(arr));
+		}
+		System.out.println("=====print graph end=====" + line.size());
+	}
+
+	static void print_v() {
+		System.out.println("====print v start======");
+		for (String arr : v) {
+			System.out.println(arr );
+		}
+		System.out.println("=====print v end=====" + line.size());
+	}
+	
+	
+}
