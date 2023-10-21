140574992a0092a27f35382dcfaddfbffe8223d2
limdongsun0814 <118763659+limdongsun0814@users.noreply.github.com>
2023-10-21 22:34:21 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2204 ms, Memory: 173112 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,58 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	static int[][] map=new int[100][100];
+	static boolean[] visit;
+	static int N;
+	static int result = 0;
+	static HashMap<Integer, ArrayList<Integer>> graph;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		visit=new boolean[N+1];
+		make_graph(br);
+		HashMap<Integer,Integer> result=bfs(1,visit);
+		for(int i=2;i<=N;i++) {
+			System.out.println(result.get(i));
+		}
+	}
+	
+	static void make_graph(BufferedReader br) throws IOException {
+		graph = new HashMap<Integer, ArrayList<Integer>>();
+		for(int i=0;i<N+1;i++) {
+			graph.put(i, new ArrayList<Integer>());
+		}
+		for(int i=0;i<N-1;i++) {
+			String[] data_arr=br.readLine().split(" ");
+			graph.get(Integer.parseInt(data_arr[0])).add(Integer.parseInt(data_arr[1]));
+			graph.get(Integer.parseInt(data_arr[1])).add(Integer.parseInt(data_arr[0]));
+		}
+	}
+	static HashMap<Integer,Integer> bfs(int V,boolean[] visit) {
+		Queue<String> q = new LinkedList<String>();
+		HashMap<Integer,Integer> result = new HashMap<Integer,Integer>();
+		q.add(V+","+1);
+		visit[V]=true;
+		while (!q.isEmpty()) {
+			String data = q.poll();
+			result.put(Integer.parseInt(data.split(",")[0]), Integer.parseInt(data.split(",")[1]));
+			Integer val = Integer.parseInt(data.split(",")[0]);
+			//graph.get(val);
+			for(int nextdata : graph.get(val)) {
+				if(!visit[nextdata]) {
+					visit[nextdata]=true;
+					q.add(nextdata+","+val);
+				}
+			}
+		}
+		return result;
+	}
+}
