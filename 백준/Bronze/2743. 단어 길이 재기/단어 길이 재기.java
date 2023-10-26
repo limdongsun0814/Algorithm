d4408f475ea52abdac3d931eb82d747a9a7c4803
limdongsun0814 <118763659+limdongsun0814@users.noreply.github.com>
2023-10-26 09:11:01 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+public class Main {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner sc = new Scanner(System.in);
+		String data_str = sc.nextLine();
+		System.out.println(data_str.length());
+
+	}
+}
