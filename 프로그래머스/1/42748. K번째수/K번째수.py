383a71df1e599bfbb4fe3474e27d98ddc80f9b08
limdongsun0814 <118763659+limdongsun0814@users.noreply.github.com>
2024-03-25 16:40:11 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for com in commands:
+        arr = array[com[0]-1:com[1]]
+        arr.sort()
+        answer.append(arr[com[2]-1])
+    return answer
