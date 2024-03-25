1cebf8d1c1958d01c1c189dcaae80df1f035cbe1
limdongsun0814 <118763659+limdongsun0814@users.noreply.github.com>
2024-03-26 00:03:14 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 1544 ms, Memory: 385556 KB -BaekjoonHub

---
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt( br.readLine());
+		for(int iiiiiii=0;iiiiiii<T;iiiiiii++) {
+			int size = Integer.parseInt( br.readLine());
+			PriorityQueue<Integer> minValue = new PriorityQueue<>();
+			PriorityQueue<Integer> maxValue = new PriorityQueue<>((o1, o2) -> o2-o1);
+			String[] data = br.readLine().split(" ");
+			for(String i: data) {
+				minValue.add(Integer.parseInt(i));
+				maxValue.add(Integer.parseInt(i));
+			}
+			System.out.println(minValue.poll()+" "+maxValue.poll());
+		}
+	}
+}
