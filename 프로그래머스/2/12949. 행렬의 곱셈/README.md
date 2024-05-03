6cc21960854898f84f3bcf34eff26983bc0978b4
limdongsun0814 <118763659+limdongsun0814@users.noreply.github.com>
2024-05-03 16:04:29 +0900

    [level 2] Title: 행렬의 곱셈, Time: 6.56 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
 
 ### 성능 요약
 
-메모리: 28.8 MB, 시간: 0.32 ms
+메모리: 10.5 MB, 시간: 6.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 02일 15:03:34
+2024년 05월 03일 16:04:29
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,7 +1,12 @@
-import numpy as np
-
 def solution(arr1, arr2):
-    arr1=np.array(arr1)
-    arr2=np.array(arr2)
-    answer=np.dot(arr1,arr2).tolist()
+    n1,m1 = len(arr1),len(arr1[0])
+    m2,n2 = len(arr2[0]),len(arr2)
+    answer = [[0 for i in range(m2)] for j in range(n1)]
+    
+    for i in range(n1):
+        for j in range(m2):
+            sumValue=0
+            for k in range(m1):
+                sumValue+=arr1[i][k]*arr2[k][j]
+            answer[i][j]=sumValue
     return answer
